f7e1c691bbf93482df422c82aee88a0d776b8e22
Mohit Gupta <cse.mohitgupta.07@gmail.com>
2025-03-19 13:11:59 +0530

    Add cliphit formula

--- Formula/cliphit.rb
+++ Formula/cliphit.rb
@@ -1,31 +1,32 @@
-class ClipHit < Formula
+class Cliphit < Formula
   desc "A clipboard history manager for macOS"
-  homepage "https://github.com/yourusername/cliphit"
-  url "https://github.com/yourusername/cliphit/releases/download/v1.0.0/cliphit-1.0.0.tar.gz"
-  sha256 "YOUR_SHA256_HASH_WILL_GO_HERE_AFTER_BUILDING"
+  homepage "https://github.com/mohitgupta07/cliphit"
+  url "https://github.com/mohitgupta07/cliphit/archive/refs/tags/v1.0.1.tar.gz"
+  # The SHA256 hash should be generated when creating a release
+  # To generate: `shasum -a 256 cliphit-1.0.1.tar.gz`
+  sha256 "4aeddcac49f33a81ddf036357f58995c6c1d224a1054251d4d505a728c218801" # Will be replaced during release process
   license "MIT"
 
   depends_on "node"
+  depends_on "yarn"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    bin.install_symlink Dir["#{libexec}/bin/*"]
-    
-    # Move the app to the Applications directory
-    prefix.install "ClipHit.app"
+    system "yarn", "install"
+    system "yarn", "package"
+    prefix.install "dist/ClipHit-darwin-x64/ClipHit.app"
   end
 
   def caveats
     <<~EOS
-      ClipHit.app was installed to:
-        #{prefix}
-
-      To link the app to your Applications folder, run:
+      ClipHit has been installed to:
+        #{prefix}/ClipHit.app
+      
+      You can link it to your Applications folder:
         ln -sf "#{prefix}/ClipHit.app" "/Applications/ClipHit.app"
     EOS
   end
 
   test do
-    assert_predicate "#{prefix}/ClipHit.app", :exist?
+    system "#{bin}/electron", "--version"
   end
 end 
